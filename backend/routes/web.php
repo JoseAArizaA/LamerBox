1dd6fcb09081749927a588b29effd5a822b4b999
Miguel Torres <myketorres563@gmail.com>
2026-04-10 19:03:35 +0200

    Your current version of Antigravity is out of date. Please visit https://antigravity.google/download to download and install the latest version.

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -1,7 +1,16 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MovieController;
+use App\Http\Controllers\FavoriteController;
+use App\Http\Controllers\WatchlistController;
+use App\Http\Controllers\ReviewController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('movies', MovieController::class);
+Route::resource('favorites', FavoriteController::class);
+Route::resource('watchlist', WatchlistController::class);
+Route::resource('reviews', ReviewController::class);
